0f7070b13c8962b66fad0d81cb57cbc66b9a2397
Administrador <Administrador@2DAM01>
2023-11-15 19:11:40 +0100

    cambio 2

--- src/Cosa2.java
+++ src/Cosa2.java
@@ -1,9 +1,12 @@
+import java.nio.file.FileSystems;
 
 public class Cosa2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola holita");
+		System.out.println("Adios Clase");
+		System.out.println("y ahora a casa");
 	}
 
 }
